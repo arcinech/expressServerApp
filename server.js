92c54631785f8fee6405af619f5b3e6d439b5eb8
Marcin Lech <arcinech@gmail.com>
2022-07-07 20:08:15 +0200

    Add: post endpoint

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLa
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -21,6 +22,19 @@ app.get('/contact', (req, res) => {
   res.render('contact');
 });
 
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message) {
+    res.render('contact', { isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
+
 app.get('/info', (req, res) => {
   res.render('info');
 });
@@ -32,6 +46,8 @@ app.get('/history', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
+
+
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
